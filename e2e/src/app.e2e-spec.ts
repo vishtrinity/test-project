ea3efd87ba5dd1c600f41642a8c9765fb89b1fc2
vishtrinity <63848400+vishtrinity@users.noreply.github.com>
2025-06-04 12:54:38 +0530

    Rename project and update UI to movie theme

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('contactBook app is running!');
+    expect(page.getTitleText()).toEqual('my-netflix app is running!');
   });
 
   afterEach(async () => {
